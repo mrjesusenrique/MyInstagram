b81a4473073716bcf42cb0412cecf1b94f25d604
Jesús Casañas <jesusenrique9773@gmail.com>
2024-10-15 23:18:15 -0300

    fix manifest

--- js/index.js
+++ js/index.js
@@ -19,21 +19,41 @@ window.addEventListener("load", () => {
   fileInput.addEventListener("change", () => {
     if (fileInput.files.length > 0) {
       const file = fileInput.files[0];
+      const reader = new FileReader();
 
-      new Compressor(file, {
-        success(compressedFile) {
-          const reader = new FileReader();
+      reader.onloadend = () => {
+        const image = new Image();
 
-          reader.onloadend = () => {
-            sessionStorage.setItem("selectedImage", reader.result);
-            window.location.href = "camera.html";
-          };
-          reader.readAsDataURL(compressedFile);
-        },
-        error(err) {
-          console.error("Error al comprimir la imagen:", err);
-        },
-      });
+        image.onload = () => {
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
+
+          canvas.width = image.width;
+          canvas.height = image.height;
+          ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+          let quality = 0.3;
+          let compressedDataUrl = canvas.toDataURL("image/webp", quality);
+
+          while (getImageSizeInKB(compressedDataUrl) > 200 && quality > 0.1) {
+            quality -= 0.1;
+            compressedDataUrl = canvas.toDataURL("image/webp", quality);
+          }
+
+          sessionStorage.setItem("selectedImage", compressedDataUrl);
+          window.location.href = "camera.html";
+        };
+
+        image.src = reader.result;
+      };
+
+      reader.readAsDataURL(file);
     }
   });
+
+  function getImageSizeInKB(dataURL) {
+    const base64String = dataURL.split(",")[1];
+    const binaryString = atob(base64String);
+    return Math.round(binaryString.length / 1024);
+  }
 });
